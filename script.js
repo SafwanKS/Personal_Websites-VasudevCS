ec7c9534cff66f9b77bbce93fbd4451023cb337c
SafwanKS <144458370+SafwanKS@users.noreply.github.com>
2023-09-16 10:04:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {
+    myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("headText").style.display = 'block'
+    } else {
+        document.getElementById("headText").style.display = 'none'
+    }
+}
